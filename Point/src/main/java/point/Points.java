54ce7e58b78bb8b7f14b90c48449bcb0a905eddd
janani-ks <123439325+janani-ks@users.noreply.github.com>
2023-01-31 15:57:18 +0530

    Update Points.java

--- Point/src/main/java/point/Points.java
+++ Point/src/main/java/point/Points.java
@@ -3,7 +3,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 class BasicPoint implements Cloneable {
 	static Logger l = Logger.getLogger("com.api.jar");
-	String x,y;
+	String x;
+	String y;
 	BasicPoint(String x1,String y1){
 		this.x=x1;
 		this.y=y1;
