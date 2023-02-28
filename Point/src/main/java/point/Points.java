f1c01a2290d360000cbd0ac5e1c1aa03a547d714
janani-ks <123439325+janani-ks@users.noreply.github.com>
2023-02-28 15:42:46 +0530

    Update Points.java

--- Point/src/main/java/point/Points.java
+++ Point/src/main/java/point/Points.java
@@ -4,15 +4,15 @@ import java.io.FileOutputStream;
 import java.io.PrintStream;
 import java.util.Scanner;
 public class Points implements Cloneable {
-	static double x;
-	static double y;
+	double x;
+	double y;
 	static Scanner scan = new Scanner(System.in);
 	static PrintStream display=new PrintStream((new FileOutputStream(FileDescriptor.out)));
 	Points(double axis1,double axis2){
 		x=axis1;
 		y=axis2;
 	}
-	static boolean equals() {
+	boolean equals() {
 		boolean b;
 		if(x==y)
 			b = true;
@@ -23,21 +23,22 @@ public class Points implements Cloneable {
 	public Object clone()throws CloneNotSupportedException{  
 		return super.clone();  
 	}  
-	static void cloneobj(Points obj)throws CloneNotSupportedException{
+	static Points cloneobj()throws CloneNotSupportedException{
 		display.println("Enter new x and y axis : ");
 		double x = scan.nextDouble();
 		double y = scan.nextDouble();
 		Points obj1 = new Points(x,y);
-		obj = (Points)obj1.clone();
+		Points obj2 = (Points)obj1.clone();
+		return obj2;
 	}
 	public static void main(String[] args) throws CloneNotSupportedException{
 		display.println("Enter x and y axis : ");
 		double x = scan.nextDouble();
 		double y = scan.nextDouble();
 		Points obj = new Points(x,y);
-		display.println("The x and y axis are same : "+Points.equals());
-        Points.cloneobj(obj);  
-        display.println("The new x and y axis are "+Points.x +" and "+Points.y);
+		display.println("The x and y axis are same : "+obj.equals());
+        obj = Points.cloneobj();  
+        display.println("The new x and y axis are "+obj.x +" and "+obj.y);
         display.println("--------------------X---------------------");
 	}
 }
