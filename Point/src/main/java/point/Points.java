f04865753ce9647e1be9c0a0b524317da516cd60
janani-ks <123439325+janani-ks@users.noreply.github.com>
2023-02-28 15:04:45 +0530

    Update Points.java

--- Point/src/main/java/point/Points.java
+++ Point/src/main/java/point/Points.java
@@ -1,30 +1,42 @@
 package point;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-class BasicPoint implements Cloneable {
-	static Logger l = Logger.getLogger("com.api.jar");
-	String x;
-	String y;
-	BasicPoint(String x1,String y1){
-		this.x=x1;
-		this.y=y1;
+import java.io.FileDescriptor;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+public class Points implements Cloneable {
+	static double x;
+	static double y;
+	static Scanner scan = new Scanner(System.in);
+	static PrintStream display=new PrintStream((new FileOutputStream(FileDescriptor.out)));
+	Points(double x,double y){
+		this.x=x;
+		this.y=y;
+	}
+	static boolean equals() {
+		if(x==y)
+			return true;
+		else
+			return false;
 	}
 	public Object clone()throws CloneNotSupportedException{  
 		return super.clone();  
-		}  
-	static void copyobject()throws CloneNotSupportedException{
-		BasicPoint s = new BasicPoint("4.6","8");
-		l.log(Level.INFO,()-> ""+(s.x).equals(s.y)); 
-		BasicPoint s1 = (BasicPoint)s.clone();
-		l.log(Level.INFO,()-> ""+s1.x+" "+s1.y);
-}
-}
-public class Points
-{
-	public static void main(String[] args)throws CloneNotSupportedException {
-		BasicPoint.copyobject(); 
-		
-		
+	}  
+	static Points Clone(Points obj)throws CloneNotSupportedException{
+		display.println("Enter new x and y axis : ");
+		double x = scan.nextDouble();
+		double y = scan.nextDouble();
+		Points obj1 = new Points(x,y);
+		obj = (Points)obj1.clone();
+		return obj1;
+	}
+	public static void main(String[] args) throws CloneNotSupportedException{
+		display.println("Enter x and y axis : ");
+		double x = scan.nextDouble();
+		double y = scan.nextDouble();
+		Points obj = new Points(x,y);
+		display.println("The x and y axis are same : "+Points.equals());
+        obj = Points.Clone(obj);  
+        display.println("The new x and y axis are "+obj.x +" and "+ obj.y);
+        display.println("--------------------X---------------------");
 	}
 }
-
